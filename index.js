bb66e0ff406c8f9ea2f742601d89b76f5d4fd23c
Ocults <abissaldev@gmail.com>
2022-12-29 23:43:59 -0300

    add inital files

---
+++ index.js
@@ -0,0 +1,166 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.7
+
+class Sprite {
+  constructor({position, velocity, color='red'}){
+    this.position = position;
+    this.velocity = velocity;
+    this.width = 50
+    this.height = 150
+    this.lastKey
+    this.attackBox = {
+      position: this.position,
+      height:100,
+      width:50
+    }
+    this.color = color
+  }
+
+  draw(){
+    c.fillStyle = this.color
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+    // attackBox
+    c.fillStyle = 'green'
+    c.fillRect(this.attackBox.position.x, this.attackBox.position.y,this.attackBox.height, this.attackBox.width)
+  }
+
+  update(){
+    this.draw();
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height){
+      this.velocity.y = 0
+    } else this.velocity.y += gravity
+  }
+}
+
+const player = new Sprite({
+  position:{
+  x: 0,
+  y: 0
+},
+velocity:{
+  x:0,
+  y:0
+}})
+
+const enemy = new Sprite({
+  position:{
+  x: 400,
+  y: 100
+},
+velocity:{
+  x:0,
+  y:0
+},
+color:'blue'})
+
+console.log(player)
+
+const keys = {
+  a:{
+    pressed:false
+  },
+  d:{
+    pressed:false
+  },
+  w:{
+    pressed:false
+  },
+  ArrowRight:{
+    pressed: false
+  },
+  ArrowLeft:{
+    pressed: false
+  }
+}
+
+let lastKey
+
+function animate(){
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'black'
+  c.fillRect(0,0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+  player.velocity.x = 0
+  enemy.velocity.x = 0
+
+  if (keys.a.pressed && player.lastKey === 'a') {
+    player.velocity.x = -5
+  }else if (keys.d.pressed && player.lastKey === 'd') {
+    player.velocity.x = 5
+  }
+
+  if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+    enemy.velocity.x = -5
+  }else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+    enemy.velocity.x = 5
+  }
+
+  // detect colosion
+  if (player.attackBox.position.x + player.attackBox.width >= 
+    enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width){
+    console.log('go')
+  }
+}
+animate()
+
+window.addEventListener('keydown',(event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = true
+      player.lastKey = 'd'
+      break
+    case 'a':
+      keys.a.pressed = true
+      player.lastKey = 'a'
+      break
+    case 'w':
+      player.velocity.y = -20
+      break
+
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = true
+      enemy.lastKey = 'ArrowRight'
+      break
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = true
+      enemy.lastKey = 'ArrowLeft'
+      break
+    case 'ArrowUp':
+      enemy.velocity.y = -20
+      break
+  }
+  console.log(event.key)
+})
+
+window.addEventListener('keyup',(event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+  }
+
+  // enemy keys
+  switch (event.key) {
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false
+      break
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false
+      break
+  }
+})
